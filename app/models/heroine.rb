f84da3b45c7a8385b49ea189d6ef6d91e79dc7b2
Ed Putans <edputans@gmail.com>
2018-10-19 11:05:29 +0100

    assignment done

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,2 +1,15 @@
 class Heroine < ApplicationRecord
-end
+  validates :super_name, uniqueness: true
+  belongs_to :power
+
+
+#   def self.search_by_power(search_by_power)
+#     if search_by_power
+#       found_power=Power.find_by(name: search_by_power)
+#       if found_power
+#         Heroine.where(power: power)
+#       end
+#     end
+#   end
+#   Heroine.all
+ end
